6ff352693e924470b11df0d94a1f4e7b296562f4
Igor <50610898+Igor3007@users.noreply.github.com>
2025-03-10 16:49:25 +0300

    save

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,42 @@
 document.addEventListener('DOMContentLoaded', function (event) {
     if (document.querySelector('#vuecart')) {
 
-        this.vueApp = new Vue({
-            el: this.$el,
+        let vueApp = new Vue({
+            el: '#vuecart',
             data: {
 
                 json: []
 
             },
+            created: function () {
+                this.fetchData()
+            },
+            methods: {
+                fetchData() {
+                    fetch('/static/cart.json', {
+                            method: 'GET',
+                        })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            this.json = data
+                        })
+                },
+
+                getPrice: function (item) {
+                    return (Number(item.total) * Number(item.price.rub.unit)).toFixed(0)
+                },
+
+                getPriceSale: function (item) {
+                    let totalPrice = (Number(item.total) * Number(item.price.rub.unit))
+                    return (totalPrice - (totalPrice * (item.sale.percent / 100))).toFixed(0)
+                },
+            },
+
+            computed: {
+                productList: function () {
+                    return this.json
+                }
+            }
         })
 
     }
