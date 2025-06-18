bd315662075a21690147090ed9ae200780b3d37f
olg <gleo@tut.by>
2025-06-18 14:24:15 +0300

    css fix; add js to redirect with switch change;

---
+++ src/js/pages/blog.js
@@ -0,0 +1,18 @@
+const redirectors = document.querySelectorAll('[data-redirect]');
+console.log(redirectors);
+const redirectTo = (url) => {
+  setTimeout(() => {
+    window.location.href = url;
+  }, 500);
+}
+redirectors.forEach(redirect => {
+  redirect.addEventListener('change', (e) => {
+    const destination = e.target.getAttribute('data-redirect');
+    const {origin} = window.location;
+    const {checked} = e.target;
+    if (!checked) {
+      return;
+    }
+    redirectTo(`${origin}/${destination}`);
+  });
+})
