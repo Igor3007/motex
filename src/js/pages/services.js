91c62e695a8354ce822529a3ae0e54ecf4db311e
olg <gleo@tut.by>
2025-06-20 09:57:54 +0300

    apply animation for cards

--- src/js/pages/services.js
+++ src/js/pages/services.js
@@ -1,17 +1,43 @@
-if (document.querySelector('[data-slider="advantages"]')) {
-  new Splide('[data-slider="advantages"]', {
-    arrows: false,
-    pagination: false,
-    fixedWidth: '240px',
-    gap: 12,
-    mediaQuery: 'min',
-    breakpoints: {
-      575.98: {
-        gap: 16,
-      },
-      767.98: {
-        fixedWidth: '308px',
-      }
-    }
-  }).mount();
+const applyAnimation = (el) => {
+  const video = el.querySelector('video');
+
+  el.addEventListener('mouseenter', (e) => {
+    el.classList.add('playing');
+    video.play();
+  });
+
+  el.addEventListener('mouseleave', (e) => {
+    el.classList.remove('playing');
+    video.pause();video.currentTime = 0;
+  })
 }
+
+const initAnimatedCards = () => {
+  document.querySelectorAll('.card-service-a').forEach((el) => {
+    applyAnimation(el);
+  })
+};
+
+const initServicesPage = () => {
+  initAnimatedCards();
+
+  if (document.querySelector('[data-slider="advantages"]')) {
+    new Splide('[data-slider="advantages"]', {
+      arrows: false,
+      pagination: false,
+      fixedWidth: '240px',
+      gap: 12,
+      mediaQuery: 'min',
+      breakpoints: {
+        575.98: {
+          gap: 16,
+        },
+        767.98: {
+          fixedWidth: '308px',
+        }
+      }
+    }).mount();
+  }
+};
+
+document.addEventListener('DOMContentLoaded', initServicesPage);
