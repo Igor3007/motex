91c96c3602fba40ed246ff2e604fa670c6e03316
olg <gleo@tut.by>
2025-06-25 12:43:56 +0300

    tablet layout

--- src/js/pages/article.js
+++ src/js/pages/article.js
@@ -20,3 +20,35 @@ document
   .forEach(element => {
     element.addEventListener('click', sendSelectedText);
   });
+
+document
+  .querySelectorAll('[data-slider="splide-read-more"]')
+  .forEach(slider => {
+    let _slider = new Splide(slider, {
+      type: 'slide',
+      rewind: true,
+      arrows: false,
+      arrowPath: 'm24.381 3.9208-3.0832 3.0417 10.292 10.292h-31.292v4.3333h31.292l-10.292 10.292 3.0832 3.0416 15.5-15.5z',
+      pagination: false,
+      gap: 12,
+      fixedWidth: '156px',
+      autoHeight: false,
+      updateOnMove: true,
+      mediaQuery: 'min',
+      breakpoints: {
+        767.98: {
+          gap: 16,
+          fixedWidth: '308px',
+        },
+        1024: {
+          arrows: true
+        },
+        1439.98: {
+          perPage: 6,
+          perMove: 1,
+        },
+      }
+    });
+
+    _slider.mount();
+  });
