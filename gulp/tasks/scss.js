9fc18612fad84fdf6cf138e24e63b94622a32e8a
olg <gleo@tut.by>
2025-06-26 12:31:19 +0300

    fix build process; replace lib for grouping media queries

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -4,7 +4,7 @@ import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
 import cssNano from "gulp-cssnano";
 import webpCss from 'gulp-webpcss';
-import groupMediaQueries from 'gulp-group-css-media-queries';
+import sortMediaQueries from 'postcss-sort-media-queries';
 import autoprefixer from 'autoprefixer';
 import postcss from 'gulp-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
@@ -36,12 +36,12 @@ const scss = (isBuild) => {
       outputStyle: 'expanded'
     }, null))
     .pipe(plugins.replace(/@img\//g, '../images/'))
-
-    /** Группировка медиа-запросов только для production */
-    .pipe(plugins.if(isBuild, groupMediaQueries()))
-
     .pipe(plugins.if(isBuild, webpCss(webpConfig)))
-    .pipe(plugins.if(isBuild, postcss([autoprefixer(), postcssPresetEnv()])))
+    .pipe(plugins.if(isBuild, postcss([
+      autoprefixer(),
+      postcssPresetEnv(),
+      sortMediaQueries({sort:'mobile-first'})
+    ])))
 
     /** Раскомментировать если нужен не сжатый дубль файла стилей */
     // .pipe(gulp.dest(filePaths.build.css))
