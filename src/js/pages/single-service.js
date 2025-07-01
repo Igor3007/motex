40c44e4372604b70ddd6eda708ae5639c15ba15a
olg <gleo@tut.by>
2025-07-01 15:11:52 +0300

    select files functional

--- src/js/pages/single-service.js
+++ src/js/pages/single-service.js
@@ -34,8 +34,86 @@ document
         }
       }
     });
+    sliderServices.on('resize', () => {
+      sliderServices.refresh();
+    })
     sliderServices.mount();
   });
+
+const initForm = () => {
+  const formFiles = [];
+  const inputEl = document.querySelector('#files');
+  const wrapper = document.querySelector('#files-list');
+
+  let mimeType = [
+    'image/jpeg',
+    'application/x-zip-compressed',
+    'image/png',
+    'image/svg+xml',
+  ];
+
+  const getFileExtension = (filename) => (filename.slice((filename.lastIndexOf('.') - 1 >>> 0) + 2));
+
+  const filePreview = (f, i) => (
+    `<div class="file-preview" title="${f.name}" data-i="${i}">
+<div class="file-preview__icon">${getFileExtension(f.name)}</div>
+<div class="file-preview__text">${f.name}</div>
+<div class="file-preview__remove icon-cross"></div>
+</div>`);
+
+  const getPreviewList = () => (formFiles.map((f, i) => filePreview(f, i)).join(""));
+
+  const removeHandler = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const {i} = e.target.parentElement.dataset;
+    formFiles.splice(parseInt(i),1);
+    wrapper.innerHTML = getPreviewList();
+    document
+      .querySelectorAll('.file-preview__remove')
+      .forEach((el) => {
+        el.addEventListener('click', removeHandler);
+      })
+  }
+
+  inputEl.addEventListener('change', (e) => {
+    const {files} = e.target;
+
+    Array.from(files).forEach(file => {
+      if (formFiles.length > 9) {
+        alert("Достигнуто максимальное количество файлов");
+        return false;
+      }
+      if (!mimeType.includes(file.type)) {
+        alert('Не поддерживаемый тип файла ' + file.name)
+        return false;
+      }
+
+      if (file.size > 50000000) {
+        alert('Допустимы файлы не более 50мБ \n' + file.name)
+        return false;
+      }
+
+      if (formFiles.find(f => f.name === file.name)) {
+        alert('Данный файл уже добавлен: ' + file.name);
+        return false;
+      }
+
+      formFiles.push(file);
+    })
+    /* remove old elements and listeners (!) */
+    document
+      .querySelectorAll('.file-preview__remove')
+      .forEach(x => x.remove());
+    wrapper.innerHTML = getPreviewList();
+    /* add new click handlers */
+    document
+      .querySelectorAll('.file-preview__remove')
+      .forEach((el) => {
+          el.addEventListener('click', removeHandler);
+      })
+  })
+}
 
 document
   .querySelectorAll('[data-action="modal"]')
@@ -51,9 +129,11 @@ document
           .then(html => {
             popup.changeContent(html);
           })
+          .then(() => {
+            initForm();
+          })
           .catch(err => console.error(err));
       });
-
     });
   });
 
