31773cfae35e21cc05bec630f0e058ad58273135
olg <gleo@tut.by>
2025-07-11 10:49:37 +0300

    add `add to cart` methods

--- src/js/pages/discount/discount-goods.js
+++ src/js/pages/discount/discount-goods.js
@@ -23,8 +23,8 @@ const discountGoodsExt = Vue.extend({
     }
   },
   filters: {
-    separate: function (x) {
-      return parseInt(x).toLocaleString("ru");
+    currency: function (x) {
+      return parseFloat(x).toLocaleString("ru", {maximumFractionDigits: 2});
     }
   },
 
@@ -54,7 +54,7 @@ const discountGoodsExt = Vue.extend({
         this.json.products[index].qty = x;
       }
     },
-    changeQty(index, event) {
+    inputQty(index, event) {
       let x = +event.target.value.replace(/\D/g, '');
       if (x > 999) {
         x = 999
@@ -65,10 +65,58 @@ const discountGoodsExt = Vue.extend({
       event.target.value = x;
       this.json.products[index].qty = x;
     },
+    changeQty(index, event) {
+      let x = +event.target.value.replace(/\D/g, '');
+      console.log(x);
+    },
     buttonOff(event) {
       event.target
         .closest('.discount-good__combo-btn')
         .classList.toggle('active');
+    },
+    sendSingleItem(index) {
+      const {id, qty} = this.products[index];
+      if (!qty) {
+        window.STATUS.wrn("Неверное количество");
+        return;
+      }
+      const payload = {
+        id, qty
+      };
+      this.toCart(payload, "Товар добавлен в корзину");
+    },
+    sendCollection() {
+      const items = this.products
+        .filter(item => item.qty > 0)
+        .map(item => ({
+          id: item.id,
+          qty: item.qty,
+        }));
+      if (items.length > 0) {
+        this.toCart(items, "Товары добавлены в корзину");
+      } else {
+        window.STATUS.wrn("Не выбран ни один товар");
+      }
+    },
+    toCart(payload, successMsg) {
+      fetch("#", {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => {
+          if (res.status === 200) {
+            window.STATUS.msg(successMsg);
+          } else {
+            window.STATUS.err(res.statusText);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          window.STATUS.err('Извините, что-то пошло не так…');
+        });
     }
   }
 });
