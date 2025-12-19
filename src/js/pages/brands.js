71b8644c66f34f6dd51dcac88a927a93246020f3
olg <gleo@tut.by>
2025-12-19 17:02:15 +0300

    [WIP]

--- src/js/pages/brands.js
+++ src/js/pages/brands.js
@@ -1,9 +1,10 @@
 import Splide from "@splidejs/splide";
 
 (function(){
-  if (!document.querySelector('.splide.brands__popular')) return;
+  const selector = '.splide.brands__popular';
+  if (!document.querySelector(selector)) return;
 
-  const splide = new Splide('.splide.brands__popular', {
+  const splide = new Splide(selector, {
     arrowPath: 'm24.381 3.9208-3.0832 3.0417 10.292 10.292h-31.292v4.3333h31.292l-10.292 10.292 3.0832 3.0416 15.5-15.5z',
     type: "loop",
     pagination: false,
@@ -24,14 +25,29 @@ import Splide from "@splidejs/splide";
 }());
 
 (function(){
-  if (!document.querySelector('.splide.brand__slider')) return;
+  const selector = '.splide.brand__slider';
+  if (!document.querySelector(selector)) return;
 
-  const splide = new Splide('.splide.brand__slider', {
+  const splide = new Splide(selector, {
     arrowPath: 'm24.381 3.9208-3.0832 3.0417 10.292 10.292h-31.292v4.3333h31.292l-10.292 10.292 3.0832 3.0416 15.5-15.5z',
     type: "loop",
     pagination: false,
     perPage: 1,
     perMove: 1
+  });
+  splide.mount();
+}());
+
+(function(){
+  const selector = '.splide.brand__slider-cat';
+  if (!document.querySelector(selector)) return;
+
+  const splide = new Splide(selector, {
+    arrowPath: 'm24.381 3.9208-3.0832 3.0417 10.292 10.292h-31.292v4.3333h31.292l-10.292 10.292 3.0832 3.0416 15.5-15.5z',
+    pagination: false,
+    perPage: 6,
+    perMove: 1,
+    gap: 16
   });
   splide.mount();
 }());
